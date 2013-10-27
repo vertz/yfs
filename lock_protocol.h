066b2ef93fe63145b1c78229a3ea3077a97c07c3
vertz <winter333@gmail.com>
2013-10-27 10:15:22 -0700

    init

---
+++ lock_protocol.h
@@ -0,0 +1,35 @@
+// lock protocol
+
+#ifndef lock_protocol_h
+#define lock_protocol_h
+
+#include "rpc.h"
+
+class lock_protocol {
+ public:
+  enum xxstatus { OK, RETRY, RPCERR, NOENT, IOERR };
+  typedef int status;
+  typedef unsigned long long lockid_t;
+  enum rpc_numbers {
+    acquire = 0x7001,
+    release,
+    stat
+  };
+};
+
+enum lockstate_t{
+    FREE = 0,
+    LOCKED
+};
+
+struct lock_t{
+    lockstate_t state;
+    pthread_cond_t lock_cond;
+
+    lock_t(): state(FREE)
+    { 
+        pthread_cond_init(&lock_cond,NULL);
+    }
+};
+
+#endif 
